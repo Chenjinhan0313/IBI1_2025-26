e9fc16dcd1f6dddbe4df5fdef6a061ae3fcef598
Chenjinhan0313 <jinhan.25@intl.zju.edu.cn>
2026-05-09 15:26:29 +0800

    correct largest_orf.py file

--- Practical 7/largest_orf.py
+++ Practical 7/largest_orf.py
@@ -1,13 +1,22 @@
-seq='AAGAUCACUGCAAUGUGUGUGUCUGUUCUGAGAGGCUAAAAG' 
-import re
-ORF=re.findall(r'AUG.*?UAA|AUG.*?UAG|AUG.*?UGA',seq)
-print(ORF)
-max=0
-for i in ORF:
-    if len(i) > max:
-        max=len(i)
-        largest_ORF=i
-print("The largest ORF is:")
-print(largest_ORF)
-print("The length of it is:")
-print(max)
+seq='AAGAUACAUGCAAGUGGUGUGUCUGUUCUGAGAGGGCCUAAAAG'
+stop_codons = {'UAA', 'UAG', 'UGA'}
+def find_longest_orf(seq):
+    max_len = 0
+    i = 0
+    while i < len(seq) - 2:
+        # find the start codon 'AUG'
+        if seq[i:i+3] == 'AUG':
+            # Scan forward in steps of 3 (one codon) from the start codon
+            for j in range(i, len(seq) - 2, 3):
+                codon = seq[j:j+3]
+                if codon in stop_codons:
+                    # find the stop codons
+                    orf_len = j - i + 3
+                    if orf_len > max_len:
+                        max_len = orf_len
+                    break  # the first stop codon is the end of this ORF
+        i += 1
+    return max_len
+longest = find_longest_orf(seq)
+print(f"Longest ORF length: {longest} nucleotides")
+
