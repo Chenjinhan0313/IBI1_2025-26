c25071c80f2399431912876af78716dabdf4748b
Chenjinhan0313 <jinhan.25@intl.zju.edu.cn>
2026-05-11 17:23:56 +0800

    correct stop_codons.py file

--- Practical 7/stop_codons.py
+++ Practical 7/stop_codons.py
@@ -1,32 +1,4 @@
 import re
-def find_stop_codons_in_longest_ORF(seq):
-    #Define the three stop codons as a set for O(1) lookup
-    stop_codons={'TAA','TAG','TGA'}
-    #Track the longest ORF length and the stop codons found in it
-    longest_orf_length = 0
-    stop_codons_in_longest = []
-    # Scan through the entire sequence to find all possible start codons
-    # Loop until len(seq)-3 
-    for start_pos in range(len(seq) - 2):
-        if seq[start_pos:start_pos+3] == 'ATG':
-            # Scan forward in steps of 3 (codon by codon)
-            # Start from the start codon position
-            for j in range(start_pos, len(seq) - 2, 3):
-                codon = seq[j:j+3]
-                if codon in stop_codons:
-                    # Calculate ORF length (from start to end of stop codon)
-                    orf_length = j - start_pos + 3
-                    if orf_length > longest_orf_length:
-                        longest_orf_length = orf_length
-                        stop_codons_in_longest = [codon]# Start new list with this stop codon
-                    elif orf_length == longest_orf_length:
-                        if codon not in stop_codons_in_longest:
-                            stop_codons_in_longest.append(codon)
-                    # Stop scanning this ORF (first stop codon encountered)
-                    break
-# Return sorted unique stop codons (set ensures uniqueness)
-    return sorted(set(stop_codons_in_longest))
-# Open input FASTA file for reading and output file for writing                       
 with open('Practical 7/yeast.fa', 'r') as f_in, open('Practical 7/stop_genes.fa', 'w') as f_out:
     current_gene_name = None
     current_seq_lines = []
@@ -36,22 +8,18 @@ with open('Practical 7/yeast.fa', 'r') as f_in, open('Practical 7/stop_genes.fa'
         if re.search(r'^>',line):
             if current_gene_name is not None and current_seq_lines:
                 seq = ''.join(current_seq_lines)
-                stops = find_stop_codons_in_longest_ORF(seq)
+                # Find which stop codons appear at least once in the sequence
+                stops = []
+                if 'TAA' in seq:
+                    stops.append('TAA')
+                if 'TAG' in seq:
+                    stops.append('TAG')
+                if 'TGA' in seq:
+                    stops.append('TGA')
                 if stops:
                     f_out.write(f'>{current_gene_name} {" ".join(stops)}\n')
                     f_out.write(seq + '\n')
-            
-                for i in current_seq_lines:
-                    seq+=i
-                stops=[]
-                start=seq.find('ATG')
-                for i in range(start, len(seq)-2, 3):
-                    codon = seq[i:i+3]
-                    if codon in ['TAA', 'TAG', 'TGA']:
-                        stops.append(codon)
-                if stops:
-                    f_out.write(f'>{current_gene_name} {" ".join(sorted(stops))}\n')
-                    f_out.write(seq + '\n')
+            # Extract gene name from header line
             match = re.search(r'gene:(\S+)', line)
             if match:
                 current_gene_name = match.group(1)
@@ -65,7 +33,13 @@ with open('Practical 7/yeast.fa', 'r') as f_in, open('Practical 7/stop_genes.fa'
     #handle the last gene in the file (after the loop ends)
     if current_gene_name is not None and current_seq_lines:
         seq = ''.join(current_seq_lines)
-        stops = find_stop_codons_in_longest_ORF(seq)
+        stops = []
+        if 'TAA' in seq:
+                    stops.append('TAA')
+        if 'TAG' in seq:
+                    stops.append('TAG')
+        if 'TGA' in seq:
+                    stops.append('TGA')
         if stops:
             f_out.write(f'>{current_gene_name} {" ".join(stops)}\n')
             f_out.write(seq + '\n')
